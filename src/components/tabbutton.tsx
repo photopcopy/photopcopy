a7e9bbe9d32170c84854c3638dfe32eae693abf6
owokoyo <photopcopy@gmail.com>
2021-07-02 01:58:39 -0700

    attempts

--- src/components/tabbutton.tsx
+++ src/components/tabbutton.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react"
 
-function TabButton(props: React.PropsWithChildren<{}>){
+function TabButton(props: React.PropsWithChildren<{}>) {
 	const [isOver, updateIsOver] = useState(false);
-	return <button onMouseEnter={()=>{updateIsOver(true)}} onMouseLeave={()=>{updateIsOver(false)}} style={{boxShadow: isOver?"0 8px 16px rgba(0,0,0,0.24)":"", transition: "box-shadow: .5s"}}>
+	return <button onMouseEnter={() => { updateIsOver(true) }} onMouseLeave={() => { updateIsOver(false) }} style={{ boxShadow: isOver ? "0 8px 16px rgba(0,0,0,0.24)" : "", width: "100%", height: 20, textAlign: "left", transition: "box-shadow: .5s" }}>
 		{props.children}
 	</button>
+
 }
 
-export {TabButton}
+
+export { TabButton }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,21 @@
-import React from "react";
+import React from 'react'
 import Head from 'next/head'
-import { Popup as PopupContainer } from "../components/popup";
-import { Settings, ComputedSettings } from "../scripts/settings";
-import { Icon } from "../components/icon";
+import { Popup as PopupContainer } from '../components/popup'
+import { Settings, ComputedSettings } from '../scripts/settings'
+import { Icon } from '../components/icon'
+import { TabButton } from '../components/tabbutton'
 
-//apparently photop is all the same page which is really weird but /shrug
+// apparently photop is all the same page which is really weird but /shrug
 
-function Main(){
+function Main() {
 	return <div key="mainContent">
-
+		
 	</div>
 }
 
-function App(){
+function App() {
 	const [state, setState] = React.useState<ComputedSettings>(Settings.currentState)
-	Settings._update = setState;
+	Settings._update = setState
 
 	return <>
 		<style jsx global>{`
@@ -34,17 +35,17 @@ function App(){
 		<noscript>
 			<h1>Enable javascript retard.</h1>
 			<a href="https://www.nhentai.net/g/364624">Click me for a surprise</a>
-			<span style={{display:"none"}}>Just kidding dont click it lmao</span>
+			<span style={{ display: 'none' }}>Just kidding dont click it lmao</span>
 		</noscript>
 		<Main></Main>
 		<PopupContainer>
-			<div key="settingsMenu" style={{width: "100%", height: "100%", backgroundColor: state.backgroundColorSecondary, borderRadius: 8, overflow: "hidden"}}>
-				<div key="topbar" style={{userSelect:"none", width: "100%", height: 60, display:"flex", alignItems: "center", boxSizing: "border-box", padding: 10, backgroundColor: state.backgroundColorTertiary, borderBottom: `solid ${state.backgroundColorQuaternary}`}}>
-					<div style={{width: "100%", height: "100%", fontSize: 40}}>Settings</div>
-					<div key="topbarRight" style={{float: "right"}}><Icon type="close"></Icon></div>
+			<div key="settingsMenu" style={{ width: '100%', height: '100%', backgroundColor: state.backgroundColorSecondary, borderRadius: 8, overflow: 'hidden', display: 'flex', flexDirection: 'column' }}>
+				<div key="topbar" style={{ userSelect: 'none', width: '100%', height: 60, display: 'flex', alignItems: 'center', boxSizing: 'border-box', padding: 10, backgroundColor: state.backgroundColorTertiary, borderBottom: `solid ${state.backgroundColorQuaternary}` }}>
+					<div style={{ width: '100%', height: '100%', fontSize: 40 }}>Settings</div>
+					<div key="topbarRight" style={{ float: 'right' }}><Icon type="close"></Icon></div>
 				</div>
-				<div style={{width: 150, height: "100%", backgroundColor: state.backgroundColorTertiary, borderRight: `solid ${state.backgroundColorQuaternary}`}}>
-
+				<div style={{ width: 150, height: '100%', backgroundColor: state.backgroundColorTertiary, borderRight: `solid ${state.backgroundColorQuaternary}` }}>
+					<TabButton>Click Me</TabButton>
 				</div>
 			</div>
 		</PopupContainer>
